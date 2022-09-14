c6ee221c70837ac99675a9aea05088ef0aabe014
Ryan McKay <ryan.mckay@shippo.com>
2022-09-14 15:15:12 -0500

    GREEN: compare similar labels

--- tests/test_compare.py
+++ tests/test_compare.py
@@ -10,3 +10,14 @@ def test_compare_label_to_itself():
     label1 = cv2.cvtColor(label1, cv2.COLOR_BGR2GRAY)
 
     assert compare_images(label1, label1, "Label1 vs Label1") == 1
+
+
+def test_compare_similar_labels():
+    label1 = cv2.imread("images/label1.png")
+    label2 = cv2.imread("images/label2.png")
+
+    # convert the image to grayscale
+    label1 = cv2.cvtColor(label1, cv2.COLOR_BGR2GRAY)
+    label2 = cv2.cvtColor(label2, cv2.COLOR_BGR2GRAY)
+
+    assert compare_images(label1, label2, "Label1 vs Label2") > 0.97
