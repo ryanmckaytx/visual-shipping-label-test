7ca0cad008a159233f6f12ef6ba6a37388b61098
Ryan McKay <ryan.mckay@shippo.com>
2022-09-15 17:49:41 -0500

    display barcode bbox

--- tests/test_barcode.py
+++ tests/test_barcode.py
@@ -3,6 +3,7 @@ import cv2
 from tests.test_utils import image_file_path as img_path
 from visual_label_compare.image_util import apply_mask
 from pyzbar.pyzbar import decode
+import matplotlib.pyplot as plt
 
 
 def test_barcode():
@@ -18,3 +19,21 @@ def test_barcode():
     # THEN
     assert len(detected_barcodes) == 1
     assert str(detected_barcodes[0].data.decode("utf-8")).endswith("92055901755477000000000015")
+    display_detected_barcode(masked1a, detected_barcodes[0])
+
+
+def display_detected_barcode(src_image, barcode):
+    (x, y, w, h) = barcode.rect
+
+    img = src_image.copy()
+    # draw the barcode bounding box
+    cv2.rectangle(img, (x - 10, y - 10),
+                  (x + w + 10, y + h + 10),
+                  (0, 220, 0), 4)
+
+    # setup the figure
+    fig = plt.figure(figsize=(5, 4), dpi=300)
+    plt.suptitle("Detected Barcode")
+    plt.imshow(img)
+    plt.axis("off")
+    plt.show()
