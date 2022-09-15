4c21879174b358fad0bc28ae1422adc5648008d7
Ryan McKay <ryan.mckay@shippo.com>
2022-09-15 17:56:30 -0500

    add another mask test

--- tests/test_diff.py
+++ tests/test_diff.py
@@ -37,3 +37,15 @@ def test_diff_somewhat_similar_labels():
     ssim = diff_images(label1a, "Label1a", label2, "Label2")
     assert ssim >= 0.97
     assert ssim < 0.99
+
+
+def test_diff_somewhat_similar_labels_with_mask():
+    label1a = cv2.imread(img_path("label1a.png"))
+    label2 = cv2.imread(img_path("label2.png"))
+    mask = cv2.imread(img_path("label-mask.png"), cv2.IMREAD_GRAYSCALE)
+    masked1a = apply_mask(label1a, mask)
+    masked2 = apply_mask(label2, mask)
+
+    ssim = diff_images(masked1a, "MaskedLabel1a", masked2, "MaskedLabel2")
+    assert ssim >= 0.98
+    assert ssim < 0.99
