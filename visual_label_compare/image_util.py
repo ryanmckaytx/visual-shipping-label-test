2e0912b648a4be97b023714d379a4e2b8a304425
Ryan McKay <ryan.mckay@shippo.com>
2022-09-15 15:58:15 -0500

    add invert param to apply_mask

--- visual_label_compare/image_util.py
+++ visual_label_compare/image_util.py
@@ -1,5 +1,5 @@
-def apply_mask(image, mask):
+def apply_mask(image, mask, invert=False):
 	result = image.copy()
-	result[mask == 0] = 0
-	result[mask != 0] = image[mask != 0]
+	result[(mask == 0) != invert] = 0
+	result[(mask != 0) != invert] = image[(mask != 0) != invert]
 	return result
